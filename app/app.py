ba86bfbdfddc774a9788c0f52f42a71ba04f17d7
dougpng <nldj265@gmail.com>
2025-07-12 11:19:46 -0300

    Gitignore funcionando e maais alguns arquivos adicionados

--- app/app.py
+++ app/app.py
@@ -1,24 +1,30 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-from app.key_app import keyW
+import os
+
+OPENWEATHER_API_KEY = os.getenv('KEY_API')
+OPENWEATHER_API_URL = "https://api.openweathermap.org/data/3.0/onecall"
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html', title='Weather')
+@app.route('/home')
 
-@app.route('/app/weathermap/')
+def index():
+    return render_template('home.html', title='Home')
+
+@app.route('/weatherapp')
 def weathermap():
+
+    
     lat = request.args.get('lat')
     lon = request.args.get('lon')
     dt = request.args.get('dt')
 
-    key = keyW()
     if not lat and not lon:
         return ("error", 400)
 
-    api_url = f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={dt}&appid={key}"
+    api_url = f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={lat}&lon={lon}&dt={dt}&appid={api_key}"
+    
     try:
         response = requests.get(api_url)
         response.raise_for_status()
@@ -26,6 +32,9 @@ def weathermap():
 
     except Exception as e:
         print(f"erro: {e}")
+    
+    return render_template('weather.html', title='Weather')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
