91960741cf18cf4d503b8139f51c3f2ad68b8c6e
dougpng <nldj265@gmail.com>
2025-07-12 16:30:21 -0300

    Remoção de arquivos temporarios

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-api_url = "https://api.openweathermap.org/data/3.0/onecall?lat={-16.3262}&lon={-48.9515}&appid={d3a661a2b87ef2a9a1b9e28c9e6c9dad}"
-
-response = requests.get(api_url)
-print (response)
-
-
